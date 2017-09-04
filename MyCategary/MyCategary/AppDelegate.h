47fabdb72a8bd5149b29339d029af08e46116919
CL <1260807001@qq.com>
2017-09-04 16:06:05 +0800

    初始化

---
+++ MyCategary/MyCategary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCategary
+//
+//  Created by HaoShiZai on 2017/9/4.
+//  Copyright © 2017年 kajiter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCategary/MyCategary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCategary
+//
+//  Created by HaoShiZai on 2017/9/4.
+//  Copyright © 2017年 kajiter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
